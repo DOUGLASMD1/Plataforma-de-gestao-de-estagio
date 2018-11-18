0c097159868b20ad8deaed3cede4351e876bc6f0
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-18 14:03:50 -0300

    Alterado configurações na estrutura do projeto.

--- WebService/config/app.php
+++ WebService/config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'pt-br',
 
     /*
     |--------------------------------------------------------------------------
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        geekcom\ValidatorDocs\ValidatorProvider::class,
 
         /*
          * Package Service Providers...
