67f0009b23df035cbcb861677ced9dcc6e03a3d8
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-18 16:12:12 -0300

    Finalizado as rotas de cadastro de usuarios.

--- WebService/routes/api.php
+++ WebService/routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 
 Route::post('/login', 'Api\UserController@login');
 
+
 Route::middleware('auth:api')->group( function () {
-	Route::post('/register-coordenador', 'Api\CoordenadorController@register');
-    Route::post('/register-aluno', 'Api\AlunoController@register');
+    Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+	Route::post('/register-aluno', 'Api\AlunoController@register');
     Route::post('/register-supervisor', 'Api\SupervisorController@register');
     Route::get('/user', 'Api\UserController@userDetails');
 });
-        
