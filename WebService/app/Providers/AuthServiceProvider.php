f92b7b2859306c96a0dad3e7ebbf2d21ff3af23b
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-20 08:57:36 -0300

    Alterado arquivo de configuração.

--- WebService/app/Providers/AuthServiceProvider.php
+++ WebService/app/Providers/AuthServiceProvider.php
@@ -25,7 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
         Passport::routes();
-        //
+        
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
     }
 }
