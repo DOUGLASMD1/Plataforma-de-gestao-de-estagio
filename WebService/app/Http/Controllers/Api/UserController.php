ec02cd20555816e18988e611eb2ef91a45323044
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-20 14:24:59 -0300

    Removido importe não necessario.

--- WebService/app/Http/Controllers/Api/UserController.php
+++ WebService/app/Http/Controllers/Api/UserController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
--- WebService/app/Models/User.php
+++ WebService/app/Models/User.php
@@ -69,8 +69,8 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
-
+	];
+	
 	public function role()
 	{
 		return $this->belongsTo(\App\Models\Role::class, 'roles_idrole');
