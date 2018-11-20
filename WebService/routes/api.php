a57fabe4e8901dd3afa51b88f8c8db05b3a063c9
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-20 16:42:48 -0300

    Adicionado novas rotas.

--- WebService/routes/api.php
+++ WebService/routes/api.php
@@ -4,9 +4,24 @@ use Illuminate\Http\Request;
 
 Route::post('/login', 'Api\UserController@login');
 
+//Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+
 Route::middleware('auth:api')->group( function () {
     Route::post('/register-coordenador', 'Api\CoordenadorController@register');
 	Route::post('/register-aluno', 'Api\AlunoController@register');
     Route::post('/register-supervisor', 'Api\SupervisorController@register');
+
+    Route::post('/register-vaga', 'Api\SupervisorController@register');
+    Route::put('/update-vaga', 'Api\SupervisorController@register');
+    Route::put('/update-estagio', 'Api\SupervisorController@register');
+    Route::put('/update-status-vaga', 'Api\SupervisorController@register');
+    Route::post('/register-frequencia', 'Api\SupervisorController@register');
+    Route::put('/update-frequencia', 'Api\SupervisorController@register');
+    Route::post('/registe-aluno-vaga', 'Api\SupervisorController@register');
+    Route::put('/update-aluno-vaga', 'Api\SupervisorController@register');
+    Route::post('/submit-file', 'Api\SupervisorController@register');
+    
     Route::get('/user', 'Api\UserController@userDetails');
 });
+
+
