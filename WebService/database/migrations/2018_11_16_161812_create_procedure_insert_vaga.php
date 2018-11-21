53772dc4b91995d2916c95cc9a467651da997dab
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-20 21:38:31 -0300

    Adicionado retorna de id.

--- WebService/database/migrations/2018_11_16_161812_create_procedure_insert_vaga.php
+++ WebService/database/migrations/2018_11_16_161812_create_procedure_insert_vaga.php
@@ -13,11 +13,17 @@ class CreateProcedureInsertVaga extends Migration
      */
     public function up()
     {
-        DB::unprepared('DROP PROCEDURE IF EXISTS insert_vagas ;
-        create procedure insert_vagas(TITULO VARCHAR(100), AREA VARCHAR(100), REQUISITOS TEXT, SUPERVISOR VARCHAR(45))
-    begin     
+        DB::unprepared('DROP FUNCTION IF EXISTS insert_vagas ;
+        create FUNCTION insert_vagas(TITULO VARCHAR(100), AREA VARCHAR(100), REQUISITOS TEXT, SUPERVISOR VARCHAR(45))
+        RETURNS INT
+        begin 
+        DECLARE IDVAGA INT;    
          insert into vagas(Titulo, Area, requisitos_para_vaga, supervisor, created_at, updated_at, deleted_at) 
              values(TITULO,AREA, REQUISITOS, SUPERVISOR, NOW(), NOW(), NULL);
+
+             SELECT MAX(idVagas) INTO IDVAGA FROM vagas;
+
+             RETURN IDVAGA;
     END ;');
     }
 
