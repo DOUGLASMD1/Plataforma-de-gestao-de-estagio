da7f73a920ee32c4ebe5928ce8c65e15bf4cbfab
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-21 22:47:41 -0300

    Adicionada novas rotas da api.

--- WebService/routes/api.php
+++ WebService/routes/api.php
@@ -4,23 +4,23 @@ use Illuminate\Http\Request;
 
 Route::post('/login', 'Api\UserController@login');
 
-//Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+
 
 Route::middleware('auth:api')->group( function () {
-    Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+    //Route::post('/register-coordenador', 'Api\CoordenadorController@register');
 	Route::post('/register-aluno', 'Api\AlunoController@register');
     Route::post('/register-supervisor', 'Api\SupervisorController@register');
-
-    Route::post('/register-vaga', 'Api\SupervisorController@register');
-    Route::put('/update-vaga', 'Api\SupervisorController@register');
-    Route::put('/update-estagio', 'Api\SupervisorController@register');
-    Route::put('/update-status-vaga', 'Api\SupervisorController@register');
-    Route::post('/register-frequencia', 'Api\SupervisorController@register');
-    Route::put('/update-frequencia', 'Api\SupervisorController@register');
-    Route::post('/registe-aluno-vaga', 'Api\SupervisorController@register');
-    Route::put('/update-aluno-vaga', 'Api\SupervisorController@register');
-    Route::post('/submit-file', 'Api\SupervisorController@register');
-    
+    Route::post('/register-vaga', 'Api\VagaController@register');
+    Route::get('/vagas-supervisor/{cpf}', 'Api\SupervisorController@vagasSupervisor');
+    Route::put('/update-vaga/{idVagas}', 'Api\VagaController@updateVaga');
+    Route::put('/update-estagio/{idEstagio}', 'Api\EstagioController@updateEstagio');
+    Route::post('/register-frequencia', 'Api\FrequenciaController@register');
+    Route::put('/update-estagio/{idEstagio}', 'Api\EstagioController@updateEstagio');  
+    Route::put('/update-frequencia/{idFrequencia}', 'Api\FrequenciaController@updateFrequencia');    
+    Route::post('/registe-aluno-vaga', 'Api\AlunoController@candidateVagas');    
+    Route::post('/update-aluno-vaga', 'Api\SupervisorController@updateAlunoVaga');    
+    Route::post('/submit-file', 'Api\ArquivoController@uploadArquivo');
     Route::get('/user', 'Api\UserController@userDetails');
 });
 
