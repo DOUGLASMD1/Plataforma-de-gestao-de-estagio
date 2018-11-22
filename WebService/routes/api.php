fc239dff00e40d570b34318efa97ca111c286f32
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2018-11-21 22:50:42 -0300

    Adicionada novas rotas da api.

--- WebService/routes/api.php
+++ WebService/routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Http\Request;
 
 Route::post('/login', 'Api\UserController@login');
 
-Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+//Route::post('/register-coordenador', 'Api\CoordenadorController@register');
 
 
 Route::middleware('auth:api')->group( function () {
-    //Route::post('/register-coordenador', 'Api\CoordenadorController@register');
+    Route::post('/register-coordenador', 'Api\CoordenadorController@register');
 	Route::post('/register-aluno', 'Api\AlunoController@register');
     Route::post('/register-supervisor', 'Api\SupervisorController@register');
     Route::post('/register-vaga', 'Api\VagaController@register');
